2aff2f033f3c3275b874e427f45f378c452ee386
Gamza_is_Potato <45549920+ggomasungu@users.noreply.github.com>
2019-03-04 15:36:26 +0900

    update cont1

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 $(function() {
 	// 공지 여닫기
 	$('.btn_close_notice').click(function() {
-		$('.notice').css('display', 'none');
-		$('.btn_open_notice').css('display', 'block');
+		$('.notice').animate({top:'-5%'}, 500)
+		$('.btn_open_notice').animate({top:0}, 500)
 	});
 	$('.btn_open_notice').click(function() {
-		$(this).css('display', 'none');
-		$('.notice').css('display', 'block');
+		$(this).animate({top:'-5%'}, 500)
+		$('.notice').animate({top:0}, 500)
 	});
 });
