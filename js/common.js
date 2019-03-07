64f95a0b6e9be68479c18e74e3df4a379baa8762
Gamza_is_Potato <45549920+ggomasungu@users.noreply.github.com>
2019-03-07 15:09:39 +0900

    fix path

--- js/common.js
+++ js/common.js
@@ -9,9 +9,7 @@ $(function() {
 		$('.notice').animate({top:0}, 500)
 	});
 
-	// 백그라운드 비디오의 원활한 재생을 위한 한번 자동 새로고침
-	// 페이지 로딩이 끝나지 않았으면 새로고침 한번 더
-	// 첫로딩 로고 안보이게, 새로고침 후 로고보임+애니메이션
+	// 백그라운드 비디오를 위해 페이지 로딩이 끝나지 않았으면 새로고침 한번 더
 	window.onload = function() {
 		if(!window.location.hash) {
 			window.location = window.location + '#loaded';
