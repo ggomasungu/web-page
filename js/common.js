7a0dc66a6c05241764069bc8d0c84bb7ebf15b1c
Gamza_is_Potato <45549920+ggomasungu@users.noreply.github.com>
2019-03-13 13:04:13 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(function() {
-	//백그라운드 비디오를 위해 페이지 로딩 두번 후 애니메이션
+	//백그라운드 비디오를 위해 페이지 로딩 두번 후 애니메이션 & 공지닫기
+	var $auto_close;
 	window.onload = function () {
 		if(!window.location.hash) {
 			window.location = window.location + '#loaded';
@@ -14,16 +15,45 @@ $(function() {
 				animation: 'intro_ani 1.3s cubic-bezier(0.215, 0.610, 0.355, 1.000) both',
 				visibility: 'visible'
 			});
+			$auto_close = setTimeout($close_notice_func,5000) // 공지 자동닫기
 		}
 	}
 
-	// 공지 여닫기
-	$('.btn_close_notice').click(function() {
-		$('.notice').animate({top:'-5%'}, 500)
-		$('.btn_open_notice').animate({top:0}, 500)
+	//스크롤 이벤트 (cont2/footer)
+	$(window).scroll(function() {
+		var $scroll = $(window).scrollTop(),
+			 $cont2scroll = $('#cont2').offset().top, // 865
+			 $cont3scroll = $('#cont3').offset().top; // 약2088.7
+
+		if ($scroll < $cont2scroll-300) {
+			$('.pf_list_bg').stop(true,true);
+			$('.pf').css('opacity', 0);
+			$('.pf_list_bg').css('left','100%')
+		}
+		else if($scroll >= $cont2scroll-300 && $scroll < $cont3scroll-400) {
+			$('.pf').css('opacity', 1);
+			$('.pf_list_bg').css('left',0)
+		}
+		else if ($scroll >= $cont3scroll-400 && $scroll < $cont3scroll) {
+			$('.pf_list_bg').stop(true,true);
+			$('.pf').css('opacity', 0);
+			$('.pf_list_bg').css('left','100%');
+		}
 	});
-	$('.btn_open_notice').click(function() {
-		$(this).animate({top:'-5%'}, 500)
+
+	// 공지 여닫기
+	var $close_notice_func = function () { //공지닫기
+		clearTimeout($auto_close);
+		$('.notice').animate({top:'-5%'}, 500)
+		$('.btn_open_notice').animate({top:0}, 500);
+		$('.btn_open_notice').css('transition', '.5s');
+	}
+	$('.btn_close_notice').click($close_notice_func); //클릭 시 공지닫기
+	$('.btn_open_notice').click(function() { //클릭 시 공지열기
+		$('.auto_close').empty();
+		$('.btn_close_notice').html('<i class="fas fa-window-close"></i>');
+		$('.btn_open_notice').css('transition', 'none');
+		$(this).animate({top:'-5%'}, 500);
 		$('.notice').animate({top:0}, 500)
 	});
 
@@ -77,34 +107,7 @@ $(function() {
 		$('.bg_cc2').css('background', '#EBEB90');
 	});
 
-	//작품리스트 영역 왔을때
-	$(window).scroll(function() {
-		var $scroll = $(window).scrollTop(),
-			 $cont2scroll = $('#cont2').offset().top;
-			 $cont3scroll = $('#cont3').offset().top;
-		if ($scroll <= $cont2scroll-300) {
-			$('.pf').css('opacity', 0);
-			$('.pf_list_bg').stop(true,true);
-			$('.pf_list_bg').css({left:'100%'}	)
-		}
-		else if($scroll >= $cont2scroll-300&&$scroll <= $cont2scroll) {
-			$('.pf').css('opacity', 1);
-			
-			$('.pf_list_bg').css({left:0}	)
-		}
-		else if ($scroll >= $cont2scroll && $scroll <= $cont3scroll-400) {
-			$('.pf').css('opacity', 1);
-			
-			$('.pf_list_bg').css({left:0}	)
-		}
-		else if ($scroll >= $cont3scroll-400&&$scroll <= $cont3scroll) {
-			$('.pf').css('opacity', 0);
-			$('.pf_list_bg').stop(true,true);
-			$('.pf_list_bg').css({left:'100%'}	)
-		};
-	});	
-
-	//작품리스트 오버배경
+	//작품리스트 호버배경
 	$('.pf').each(function() {
 		$(this).hover(function() {
 			$(this).find('.pf_bg').animate({top: 0},300)
@@ -122,7 +125,7 @@ $(function() {
 		}, function() {
 			$('.bg_hover').stop(true,true);
 			$('.contact_tit').css('animation', 'none');
-			$(this).find('.bg_hover').animate({top:'430px'},500)
+			$(this).find('.bg_hover').animate({top:'100%'},500)
 		});
 	});
 });
